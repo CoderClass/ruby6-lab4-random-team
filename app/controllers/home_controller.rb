9ce2481f30d912550afb9ef5be17e478e6941ca7
Khoi Lai <khoi.geeky@gmail.com>
2017-07-05 20:04:27 +0700

    Milestone 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def login
+    session[:username] = params[:username]
+    redirect_back fallback_location: root_path
+  end
+
+  def logout
+    session[:username] = nil
+    redirect_back fallback_location: root_path
+  end
 end
